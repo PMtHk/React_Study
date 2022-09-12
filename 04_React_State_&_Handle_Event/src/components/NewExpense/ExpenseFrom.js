ae017a73a6855a919c3031da27aff30ea646183f
PMtHk <zooby88@naver.com>
2022-09-12 17:59:11 +0900

    add NewExpense

---
+++ 04_React_State_&_Handle_Event/src/components/NewExpense/ExpenseFrom.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './ExpenseFrom.css'
+
+const ExpenseForm = () => {
+  return <form>
+    <div className='new-expense__controls'>
+      <div className='new-expense__control'>
+        <label>Title</label>
+        <input type="text" />
+      </div>
+    </div>
+  </form>
+}
+
+export default ExpenseForm;
