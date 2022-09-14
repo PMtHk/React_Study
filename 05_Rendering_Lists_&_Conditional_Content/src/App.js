069bc4777553e1debd87d11a32988ccc39b6a21d
PMtHk <zooby88@naver.com>
2022-09-14 21:26:20 +0900

    adding Expense, listing new Expense

--- 05_Rendering_Lists_&_Conditional_Content/src/App.js
+++ 05_Rendering_Lists_&_Conditional_Content/src/App.js
@@ -1,45 +1,38 @@
-import Expenses from './components/Expenses/Expenses'
+import { useState } from 'react';
+import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2022, 4, 5),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 4, 9) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2019, 4, 9) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 245.24,
-      date: new Date(2022, 7, 14),
+      date: new Date(2020, 7, 14),
     },
     {
       id: 'e4',
       title: 'New Desk (Motion Desk)',
       amount: 450,
-      date: new Date(2022, 8, 22),
+      date: new Date(2021, 8, 22),
     },
-  ];
-
+  ]);
 
   const addExpenseHandler = (expenseData) => {
-    const newExpense = {
-      ...expenseData
-    }
-    console.log(newExpense, "in app");
-    expenses.push(newExpense);
-    console.log(expenses);
-  }
+    setExpenses((prev) => [expenseData, ...prev,]);
+  };
 
   return (
-    
-
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- 05_Rendering_Lists_&_Conditional_Content/src/components/Expenses/Expenses.js
+++ 05_Rendering_Lists_&_Conditional_Content/src/components/Expenses/Expenses.js
@@ -19,26 +19,9 @@ function Expenses(props) {
           selected={filterYear}
           onChangeDropdown={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((elem) => (
+          <ExpenseItem  key={elem.id} title={elem.title} amount={elem.amount} date={elem.date} />
+        ))}
       </Card>
     </div>
   );
