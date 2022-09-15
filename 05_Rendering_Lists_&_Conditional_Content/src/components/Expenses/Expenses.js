9012c571dfd084403ea95b987dce99ee59012779
PMtHk <zooby88@naver.com>
2022-09-15 17:03:50 +0900

    add conditional return phrase

--- 05_Rendering_Lists_&_Conditional_Content/src/components/Expenses/Expenses.js
+++ 05_Rendering_Lists_&_Conditional_Content/src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
+
 
 import './Expenses.css';
 
@@ -16,19 +17,6 @@ function Expenses(props) {
     return expenses.date.getFullYear() === parseInt(filterYear);
   });
 
-  let expensesContent = <p>No Expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((elem) => (
-      <ExpenseItem
-        key={elem.id}
-        title={elem.title}
-        amount={elem.amount}
-        date={elem.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
@@ -38,7 +26,7 @@ function Expenses(props) {
         />
         {/* // props.items */}
         {/* // .filter((elem) => elem.date.getFullYear() === parseInt(filterYear)) <---myAnswer */}
-      {expensesContent}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
