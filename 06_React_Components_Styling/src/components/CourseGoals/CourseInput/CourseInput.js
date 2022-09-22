b4ebf33c3eaf4fbecfb3866138176f1df1ad1554
PMtHk <zooby88@naver.com>
2022-09-22 14:41:18 +0900

    apply style with inline style

--- 06_React_Components_Styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 06_React_Components_Styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,25 +3,42 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className='form-control'>
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+        // inline Style -> very high priority...override our style in css
+          style={{
+            border: !isValid ? '1px solid red' : '1px solid grey',
+            // or borderColor: !isValid ? ...
+            background: !isValid ? 'pink' : 'white',
+          }}
+          type='text'
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
