d11ca95ee374922901508aae8dced9beb60dd07a
PMtHk <zooby88@naver.com>
2022-09-26 22:26:12 +0900

    CSS-Module Comments

--- 08_Project_1_Practice/LecturerSolution/src/components/Users/AddUser.js
+++ 08_Project_1_Practice/LecturerSolution/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 //css-module
 import classes from './AddUser.module.css'
@@ -11,6 +12,8 @@ const AddUser = (props) => {
   };
 
   return (
+    // css-module 의 경우 사용자 지정 컴포넌트의 경우 그 안에서는 props 를 통해서만 동작한다.
+    // 따라서, 상위 컴포넌트(Card.js)에서 className 속성으로 받아들여서 처리할 수 있도록 해아한다.
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
@@ -18,7 +21,7 @@ const AddUser = (props) => {
         <input id='username' type='text' />
         <label htmlFor='age'>Age (Years)</label>
         <input id='age' type='number' />
-        <button type='submit'>AddUser</button>
+        <Button type='submit'>AddUser</Button>
       </form>
     </Card>
   );
