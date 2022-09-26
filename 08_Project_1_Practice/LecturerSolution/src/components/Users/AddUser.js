a95531e82ec14ec9e77a4305e15a8b63bb3bf0a3
PMtHk <zooby88@naver.com>
2022-09-26 22:15:22 +0900

    styling Card, AddUser Components

--- 08_Project_1_Practice/LecturerSolution/src/components/Users/AddUser.js
+++ 08_Project_1_Practice/LecturerSolution/src/components/Users/AddUser.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
+import Card from '../UI/Card';
+
+//css-module
+import classes from './AddUser.module.css'
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      {/* htmlFor 는 for의 속성을 할당하는 props 이름 ...
-     for 는 js예약어라 사용할 수 없음 */}
-      <input id='username' type='text' />
-      <label htmlFor='age'>Age (Years)</label>
-      <input id='age' type='number' />
-      <button type='submit'>AddUser</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        {/* htmlFor 는 for의 속성을 할당하는 props 이름 ... for 는 js예약어라 사용할 수 없음 */}
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' />
+        <button type='submit'>AddUser</button>
+      </form>
+    </Card>
   );
 };
 
