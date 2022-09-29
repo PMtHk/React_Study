d3f70547a39372edb11744b5becf9037007a87a9
PMtHk <zooby88@naver.com>
2022-09-29 14:30:44 +0900

    add logic, state to ErrorModal

--- 08_Project_1_Practice/LecturerSolution/src/components/Users/AddUser.js
+++ 08_Project_1_Practice/LecturerSolution/src/components/Users/AddUser.js
@@ -11,15 +11,26 @@ const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
 
+  // state for modal
+  const [error, setError] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid Input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
       // 두 input 중 하나라도 비어있으면 동작 하지 않도록 함.
       return;
     }
 
     if (parseInt(enteredAge) < 1) {
+      setError({
+        title: 'Invalid Age',
+        message: 'Please enter a valid age (age must be greater than 0).',
+      });
       // 입력된 나이가 1보다 작다면 동작 하지 않도록 함.
       return;
     }
@@ -38,12 +49,18 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler =() =>{
+    setError(null); // null is falsy value
+  }
+
   return (
     // Modal이 논리적으로 컴포넌트 트리에서 가능한 한 높은 위치 (App.js) 에 위치해야한다는 주장도 있고
     // AddUser 가 트기거가 되니, AddUser에 위치해야 한다는 주장도 있다.
     // 하나를 적절한 논리에 맞게 선택하고 사용하자.
     <div>
-      <ErrorModal title="An Error Occured!" message="Something went wrong!"/>
+      {error && (
+        <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>
+      )}
       {/* // css-module 의 경우 사용자 지정 컴포넌트의 경우 그 안에서는 props 를
       통해서만 동작한다. // 따라서, 상위 컴포넌트(Card.js)에서 className
       속성으로 받아들여서 처리할 수 있도록 해아한다. */}
