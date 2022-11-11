cf1f3dbe95a3fe49e068e45a08aeeeb87fa73ccf
PMtHk <zooby88@naver.com>
2022-11-11 11:13:29 +0900

    스타일 적용 오류 수정 및 장바구니 담기 용 폼 추가

--- 11_Project_2_FoodOrderApp/src/components/Meals/AvailableMeals.js
+++ 11_Project_2_FoodOrderApp/src/components/Meals/AvailableMeals.js
@@ -8,6 +8,7 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
     return (
       <MealItem
+        id={meal.id}
         key={meal.id}
         name={meal.name}
         description={meal.description}
@@ -17,7 +18,7 @@ const AvailableMeals = () => {
   });
 
   return (
-    <section className={classes.meal}>
+    <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
       </Card>
