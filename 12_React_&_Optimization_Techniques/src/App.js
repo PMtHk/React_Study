4ae96a947950b71d69457cd16415728e231f7d06
PMtHk <zooby88@naver.com>
2022-12-28 22:50:37 +0900

    React.memo() & useCallback() with dependency

--- 12_React_&_Optimization_Techniques/src/App.js
+++ 12_React_&_Optimization_Techniques/src/App.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState();
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("App Running...")
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle((prevAllowToggle) => !prevAllowToggle);
+  }
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
-      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
