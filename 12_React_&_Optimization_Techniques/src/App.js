33cfc9bb0fa9b245d7587f5fe2af9dcba939d197
PMtHk <zooby88@naver.com>
2022-12-28 21:46:53 +0900

    컴포넌트 업데이트 시 동작 이해하기
    - 컴포넌트 함수 재실행
    - 자식 컴포넌트 함수 재실행

--- 12_React_&_Optimization_Techniques/src/App.js
+++ 12_React_&_Optimization_Techniques/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState();
+
+  console.log("App Running...")
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      <DemoOutput show={false}/>
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
 }
