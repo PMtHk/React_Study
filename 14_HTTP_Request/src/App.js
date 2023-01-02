96fd9a976b4201d51feb4d2d8f91c8ee8c4021dd
PMtHk <zooby88@naver.com>
2023-01-02 21:18:17 +0900

    use Async & Await

--- 14_HTTP_Request/src/App.js
+++ 14_HTTP_Request/src/App.js
@@ -6,25 +6,22 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
+  async function fetchMoviesHandler() {
     // Fetch API 사용. Promise 라는 객체를 반환한다.
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // http 요청으로 받은 결과와 우리가 사용하려는 형태를 맞춰야 한다.
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
 
-        setMovies(transformedMovies);
-      });
+    // http 요청으로 받은 결과와 우리가 사용하려는 형태를 맞춰야 한다.
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
   }
 
   return (
