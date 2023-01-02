9deead9e80580ee2118ff2d2e871e9cd95262b99
PMtHk <zooby88@naver.com>
2023-01-02 21:49:49 +0900

    add AddMovie Component

--- 14_HTTP_Request/src/App.js
+++ 14_HTTP_Request/src/App.js
@@ -1,28 +1,25 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false); // 로딩중인지 확인하기 위한 상태.
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    
     try {
-      // Fetch API 사용. Promise 라는 객체를 반환한다.
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch('https://swapi.dev/api/films/');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
-      
+
       const data = await response.json();
-      
-      // http 요청으로 받은 결과와 우리가 사용하려는 형태를 맞춰야 한다.
+
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -30,8 +27,7 @@ function App() {
           openingText: movieData.opening_crawl,
           releaseDate: movieData.release_date,
         };
-      }, []);
-      
+      });
       setMovies(transformedMovies);
     } catch (error) {
       setError(error.message);
@@ -42,8 +38,12 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
-  
-  let content = <p>Found no movies</p>;
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
@@ -59,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
